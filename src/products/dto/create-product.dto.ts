464837e9f13f1d58bfb90192e71449634f420854
BeomJoon-MSB <ehdfkd@benter.co.kr>
2024-04-05 17:36:45 +0900

    products post api - defualt value setting with swagger request body

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,45 +1,41 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsDate, IsNumber, IsString } from 'class-validator';
 export class CreateProductDto {
-  @ApiProperty()
+  @ApiProperty({ example: 1, description: '기본값' })
   @IsNumber()
   readonly user_id: number;
 
-  @ApiProperty()
+  @ApiProperty({ example: 123123, description: '기본값' })
   @IsNumber()
   readonly product_code: number;
 
-  @ApiProperty()
+  @ApiProperty({ example: 1, description: '기본값' })
   @IsNumber()
   readonly brand_id: number;
 
-  @ApiProperty()
+  @ApiProperty({ example: '테스트 상품', description: '기본값' })
   @IsString()
   readonly product_name: string;
 
-  @ApiProperty()
+  @ApiProperty({ example: 1, description: '기본값' })
   @IsNumber()
   readonly category_detail_id: number;
 
-  @ApiProperty()
-  @IsNumber()
+  @ApiProperty({ example: 2000, description: '기본값' })
   @IsNumber()
   readonly price: number;
 
-  @ApiProperty()
+  @ApiProperty({ example: '테스트 상품 설명', description: '기본값' })
   @IsString()
   readonly description: string;
 
-  @ApiProperty()
-  @IsNumber()
+  @ApiProperty({ example: 20, description: '기본값' })
   @IsNumber()
   readonly stock: number;
 
-  @ApiProperty()
   @IsBoolean()
   readonly is_deleted: boolean;
 
-  @ApiProperty()
   @IsNumber()
   readonly sales_count: number;
 
