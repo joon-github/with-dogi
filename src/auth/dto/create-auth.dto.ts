6616e4b226e62d926d68c25ae48eb64d9fc6c06c
BeomJoon-MSB <ehdfkd@benter.co.kr>
2024-04-08 17:33:03 +0900

    회원가입 dto 수정

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,13 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsIn, IsOptional, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsIn,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
 
 export class CreateMemberDto {
   @IsString()
+  @IsEmail()
   @ApiProperty({ example: 'tester@naver.com', description: '기본값' })
   email: string;
 
   @ApiProperty({ example: 'test', description: '기본값' })
   @IsString()
+  @MinLength(8, { message: '비밀번호는 최소 8자 이상이어야 합니다.' })
+  @Matches(/(?=.*\W)/, {
+    message: '비밀번호는 최소 하나의 특수문자를 포함해야 합니다.',
+  })
   password: string;
 
   @ApiProperty({ example: 'tester', description: '기본값' })
@@ -15,6 +27,9 @@ export class CreateMemberDto {
   name: string;
 
   @ApiProperty({ example: '010-0000-0000', description: '기본값' })
+  @Matches(/^\d{3}-\d{3,4}-\d{4}$/, {
+    message: '전화번호는 3-3(또는 4)-4 형식이어야 합니다.',
+  })
   @IsString()
   phone: string;
 
