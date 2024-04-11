5387cc41693d183c675f8529f1a781292dfd0e2e
BeomJoon-MSB <ehdfkd@benter.co.kr>
2024-04-11 10:51:19 +0900

    products controller 응답 값 수정

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -11,6 +11,8 @@ import { ProductService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ResponesContainerDto } from 'src/global/dto/respones-container.dto';
+import { Products } from './entities/products.entity';
 
 @Controller('products')
 @ApiTags('products')
@@ -18,31 +20,63 @@ export class ProductController {
   constructor(private readonly productService: ProductService) {}
   @Get()
   @ApiOperation({ summary: '상품 전체 조회' })
-  findAll() {
-    return this.productService.findAll();
+  async findAll(): Promise<ResponesContainerDto<Products[]>> {
+    const data = await this.productService.findAll();
+    return {
+      statusCode: 200,
+      message: '상품 전체 조회 성공',
+      data: data,
+    };
   }
 
   @Get(':id')
   @ApiOperation({ summary: '상품 단일 조회' })
-  async findOne(@Param('id') id: string) {
-    return await this.productService.findOne(+id);
+  async findOne(
+    @Param('id') id: string,
+  ): Promise<ResponesContainerDto<Products>> {
+    const data = await this.productService.findOne(+id);
+    return {
+      statusCode: 200,
+      message: `${id}번 상품 조회 성공`,
+      data: data,
+    };
   }
 
   @Post()
   @ApiOperation({ summary: '상품 등록' })
-  async create(@Body() createProductDto: CreateProductDto) {
-    return await this.productService.create(createProductDto);
+  async create(
+    @Body() createProductDto: CreateProductDto,
+  ): Promise<ResponesContainerDto<null>> {
+    await this.productService.create(createProductDto);
+    return {
+      statusCode: 201,
+      message: '상품 등록 성공',
+      data: null,
+    };
   }
 
   @Patch(':id')
   @ApiOperation({ summary: '상품 수정' })
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(+id, updateProductDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ): Promise<ResponesContainerDto<null>> {
+    this.productService.update(+id, updateProductDto);
+    return {
+      statusCode: 200,
+      message: `${id}번 상품 수정 성공`,
+      data: null,
+    };
   }
 
   @Delete(':id')
   @ApiOperation({ summary: '상품 삭제' })
-  remove(@Param('id') id: string) {
-    return this.productService.remove(+id);
+  async remove(@Param('id') id: string): Promise<ResponesContainerDto<null>> {
+    this.productService.remove(+id);
+    return {
+      statusCode: 200,
+      message: `${id}번 상품 삭제 성공`,
+      data: null,
+    };
   }
 }
