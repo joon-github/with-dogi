40a09e18148de23ce4caffd0a20040f0d048593d
BeomJoon-MSB <ehdfkd@benter.co.kr>
2024-04-12 11:23:34 +0900

    상품 카테고리 API 수정

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -6,18 +6,31 @@ import {
   Patch,
   Param,
   Delete,
+  Req,
 } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Request } from 'express';
+import { TokenPayload } from 'src/auth/interfaces/token-payload.interface';
+import { ResponesContainerDto } from 'src/global/dto/respones-container.dto';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoriesService.create(createCategoryDto);
+  async create(
+    @Body() createCategoryDto: CreateCategoryDto,
+    @Req() request: Request,
+  ): Promise<ResponesContainerDto<null>> {
+    const user = request['user'] as TokenPayload;
+    await this.categoriesService.create(createCategoryDto, user.user_id);
+    return {
+      statusCode: 201,
+      message: '카테고리 등록 성공',
+      data: null,
+    };
   }
 
   @Get()
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,11 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Categories } from './entities/Categories.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { BrandService } from 'src/brand/brand.service';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectRepository(Categories)
+    private categoriesRepository: Repository<Categories>,
+
+    private readonly brandService: BrandService,
+  ) {}
+  async create(createCategoryDto: CreateCategoryDto, user_id: number) {
+    const brand = await this.brandService.checkBrandOwner(
+      createCategoryDto.brand_id,
+      user_id,
+    );
+    const category = {
+      ...createCategoryDto,
+      brand,
+    };
+    return this.categoriesRepository.save(category);
   }
 
   findAll() {
