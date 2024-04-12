7c5304a4dd9ecd47a7935a54a7ace164ae036bda
joon-guthub <ehdfkd2000@naver.com>
2024-04-13 01:22:21 +0900

    swagger 수정

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,13 +1,39 @@
 import { INestApplication } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ProductModule } from './routes/products/products.module';
+import { BrandModule } from './routes/brand/brand.module';
+import { AuthModule } from './routes/auth/auth.module';
+import { CategoriesModule } from './routes/categories/categories.module';
+import { DetailModule } from './routes/categories/detail/detail.module';
 
 export function setupSwagger(app: INestApplication): void {
+  //현재 경로
+  const nav = `
+  <a href="http://localhost:8000/api-base-docs#/">기본 api</a>
+  <br/>
+  <a href="http://localhost:8000/api-products-docs">상품 api</a>
+  `;
   const options = new DocumentBuilder()
-    .setTitle('NestJS Study API Docs')
-    .setDescription('NestJS Study API description')
+    .setTitle('기본 API')
+    .setDescription(nav)
     .setVersion('1.0.0')
     .build();
 
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api-docs', app, document);
+  const document = SwaggerModule.createDocument(app, options, {
+    include: [AuthModule, CategoriesModule, DetailModule],
+  });
+
+  const productsOptions = new DocumentBuilder()
+    .setTitle('상품 API')
+    .setDescription(nav)
+    .setVersion('1.0.0')
+    .build();
+
+  const productsDocument = SwaggerModule.createDocument(app, productsOptions, {
+    include: [ProductModule, BrandModule],
+  });
+
+  SwaggerModule.setup('api-products-docs', app, productsDocument);
+
+  SwaggerModule.setup('api-base-docs', app, document);
 }
