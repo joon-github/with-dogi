126122a8d74f53a1651f7fdaafbfafcebef455c2
joon-guthub <ehdfkd2000@naver.com>
2024-04-15 00:36:02 +0900

    카테고리 에러 수정

--- src/routes/category/category.module.ts
+++ src/routes/category/category.module.ts
@@ -3,9 +3,9 @@ import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { AuthService } from 'src/routes/auth/services/auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Category } from './entities/category.entity';
 import { Members } from 'src/routes/auth/entities/Members.entity';
 import { AuthModule } from 'src/routes/auth/auth.module';
+import { Category } from './entities/Category.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Category, Members]), AuthModule],
