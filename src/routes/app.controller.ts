cb8b54d68d3f3e3a0e6a1c3d9b7d77b5328bab28
BeomJoon-MSB <ehdfkd@benter.co.kr>
2024-05-03 13:40:42 +0900

    메인페이지 url 수정

--- src/routes/app.controller.ts
+++ src/routes/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   @Get()
   getHello(@Res() res: Response): void {
     res.send(
-      '<html><body><h1>안녕하세요. with_dogi API 입니다.</h1><a href="http://localhost:8000/api-base-docs#/">docs로 이동</a></a></body></html>',
+      `<html><body><h1>안녕하세요. with_dogi API 입니다.</h1><a href="${process.env.NODE_ENV === "production" ? process.env.BASE_URL : 'http://localhost:8000'}/api-base-docs#/">docs로 이동</a></a></body></html>`,
     );
   }
 }
